673df2ba45c18799a337a8b5fe665860173773d5
tengqi <75350626+dwz92@users.noreply.github.com>
2024-03-16 11:47:25 -0400

    updating current work process: creating figures for result and data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -22,30 +22,40 @@ library(dplyr)
 # The simulated data is derived from a normal distribution instead of a poisson distribution since birth rate can be more than just integers.
 # By applying the abs() function, we guarantee the recording of only positive birth rates for the older age group.
 
-set.seed(853)
+
+word_dist <- function(auth_list) {
+  res <- tibble(
+    author = c(),
+    average =c(),
+    stddev = c()
+  )
+  
+  for (info in auth_list) {
+    avg = mean(info$word_count)
+    std = sd(info$word_count)
+    
+    res <-
+      rbind(res,
+        tibble(
+          author = info$author,
+          average = avg,
+          stddev = std
+        )
+      )
+  }
+  return(res)
+}
+
+
+cttr_dav <- read.csv(here::here("data/analysis_data/cttr_david.csv"))
+cttr_hug <- read.csv(here::here("data/analysis_data/cttr_hugh.csv"))
+cttr_nan <- read.csv(here::here("data/analysis_data/cttr_nancy.csv"))
+cttr_pow <- read.csv(here::here("data/analysis_data/cttr_powell.csv"))
 
 # Initialize a tibble
-simulated_data <- tibble(
-  year = c(1980:2020),
-  br_1519 = abs(rnorm(n = 41,mean = 50.5, sd=10.5)),
-  br_2024 = abs(rnorm(n = 41,mean = 55.5, sd=10.5)),
-  br_2529 = abs(rnorm(n = 41,mean = 80.5, sd=5.5)),
-  br_3034 = abs(rnorm(n = 41,mean = 55.5, sd=5.5)),
-  br_3539 = abs(rnorm(n = 41,mean = 35.5, sd=5.5)),
-  br_4044 = abs(rnorm(n = 41,mean = 25.5, sd=5.5)),
+word_distribution <- word_dist(
+  list(cttr_dav, cttr_hug, cttr_nan, cttr_pow)
 )
 
-simulated_data$Younger <- simulated_data |>
-  select(
-    br_1519,br_2024,br_2529
-  ) |>
-  rowMeans()
 
-simulated_data$Older <- simulated_data |>
-  select(
-    br_3034,br_3539,br_4044
-  ) |>
-  rowMeans()
-
-# View the simulated data
-print(simulated_data)
+unique(word_distribution)
